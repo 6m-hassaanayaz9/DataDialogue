4b550c88fdbe5c59ff2ce9ddc90bd46cfa667ff5
Muhammad Hassaan Ayaz <m.hassaanayaz04@gmail.com>
2024-03-24 01:55:20 +0500

    debugging

--- datadialgue_bckend/datadialgue_bckend/settings.py
+++ datadialgue_bckend/datadialgue_bckend/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -39,7 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
+    
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
